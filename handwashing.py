70248abb62f11acf38180cb92c1d465d6e5913df
Huong Tran <huong.tran0196@gmail.com>
2021-06-09 22:10:48 +0300

    Plot the proportion deaths

--- handwashing.py
+++ handwashing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 yearly = pd.read_csv('datasets/yearly_deaths_by_clinic.csv')
 print(yearly)
 
@@ -8,3 +10,7 @@ clinic_1 = yearly[yearly['clinic'] == 'clinic 1']
 clinic_2 = yearly[yearly['clinic'] == 'clinic 2']
 print(clinic_1)
 
+# Plot the yearly proportion deaths for both clinics
+ax = clinic_1.plot(x='year', y='proportion_deaths', label='Clinic 1')
+clinic_2.plot(x='year', y='proportion_deaths', label='Clinic 2', ax=ax, ylabel='Proportion deaths')
+plt.show()
