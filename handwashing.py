19986409637985a0087b941bb02b738b869f0006
Huong Tran <huong.tran0196@gmail.com>
2021-06-09 22:03:53 +0300

    Add new column & Subset rows from dataframe

--- handwashing.py
+++ handwashing.py
@@ -1,3 +1,10 @@
 import pandas as pd
 yearly = pd.read_csv('datasets/yearly_deaths_by_clinic.csv')
 print(yearly)
+
+# Calculate the proportion of deaths 
+yearly['proportion_deaths'] = yearly['deaths'] / yearly['births']
+clinic_1 = yearly[yearly['clinic'] == 'clinic 1']
+clinic_2 = yearly[yearly['clinic'] == 'clinic 2']
+print(clinic_1)
+
